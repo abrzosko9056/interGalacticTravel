59734168e0646024641232a5800e571416a717b4
Alex Brzosko <abrzosko9056@jh-mba158-01.d214.org>
2018-11-01 11:27:07 -0500

    Initial Commit

---
+++ interGalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interGalacticTravel
+//
+//  Created by Alex Brzosko on 11/1/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
