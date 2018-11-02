d7c0072a342bf71cbe71e5f5e9502a250cc20bdd
Alex Brzosko <abrzosko9056@jh-mba158-01.d214.org>
2018-11-02 11:38:27 -0500

    added imageView

---
+++ interGalacticTravel/StarViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StarViewController.swift
+//  interGalacticTravel
+//
+//  Created by Alex Brzosko on 11/2/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+
+}
